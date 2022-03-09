31d9c17de968c2a210a8124904a6a1290a14ecc7
YaoZhen <654556826@qq.com>
2022-03-09 20:59:35 +0800

    add shell

--- scripts/test/ConvE_WN.sh
+++ scripts/test/ConvE_WN.sh
@@ -1,24 +1,20 @@
 DATA_DIR=dataset
 
-MODEL_NAME=CrossE
-LITMODEL_NAME=CrossELitModel
-TRAIN_SAMPLER_CLASS=CrossESampler
-LOSS=CrossE_Loss
-DATASET_NAME=WN18
+MODEL_NAME=ConvE
+DATASET_NAME=WN18RR
 DATA_PATH=$DATA_DIR/$DATASET_NAME
-
-EVAL_BS=512
-TRAIN_BS=2048
-DIM=100
-LEARNING_RATE=0.01
-WEIGHT_DECAY=1e-4
-
-MAX_EPOCHES=1000
-CHECK_PER_EPOCH=5
-EARLY_STOP=5000
-NUM_WPRDKERS=8
-GPU=1
-
+LOSS=Cross_Entropy_Loss
+TRAIN_BS=1024
+EVAL_BS=256
+DIM=200
+LEARNING_RATE=0.001
+MAX_EPOCHES=3000
+REGULARIZATION=0
+NUM_WORKERS=8
+CHECK_PER_EPOCH=30
+LITMODEL_NAME=ConvELitModel
+TRAIN_SAMPLER_CLASS=ConvSampler
+GPU=2
 
 CUDA_VISIBLE_DEVICES=$GPU python -u main.py \
     --model_name $MODEL_NAME \
@@ -31,13 +27,13 @@ CUDA_VISIBLE_DEVICES=$GPU python -u main.py \
     --train_bs $TRAIN_BS \
     --eval_bs $EVAL_BS \
     --lr $LEARNING_RATE \
+    --num_workers $NUM_WORKERS \
     --check_per_epoch $CHECK_PER_EPOCH \
-    --weight_decay $WEIGHT_DECAY \
+    -r $REGULARIZATION \
     --litmodel_name $LITMODEL_NAME \
     --train_sampler_class $TRAIN_SAMPLER_CLASS \
     --use_wandb \
-    --early_stop_patience $EARLY_STOP \
-    
+    --save_config \
 
 
 
--- scripts/test/RGCN_FB.sh
+++ scripts/test/RGCN_FB.sh
@@ -1,40 +1,40 @@
 DATA_DIR=dataset
 
-MODEL_NAME=BoxE
+MODEL_NAME=RGCN
 DATASET_NAME=FB15K237
 DATA_PATH=$DATA_DIR/$DATASET_NAME
-LITMODEL_NAME=KGELitModel
-MAX_EPOCHS=1000
+LITMODEL_NAME=RGCNLitModel
+TRAIN_SAMPLER_CLASS=GraphSampler
+TEST_SAMPLER_CLASS=GraphTestSampler
+MAX_EPOCHES=10000
 EMB_DIM=500
-LOSS=Adv_Loss
-TRAIN_BS=1024
-EVAL_BS=16
-NUM_NEG=100
-MARGIN=12.0
-DIS_ORDER=1
-LR=2e-5
-REGULARIZATION=1e-7
-CHECK_PER_EPOCH=50
+LOSS_NAME=RGCN_Loss
+TRAIN_BS=60000
+EVAL_BS=300
+NUM_NEG=10
+LR=1e-4
+REGULARIZATION=1e-5
+CHECK_PER_EPOCH=500
 NUM_WORKERS=16
-GPU=3
+GPU=0
 
-CUDA_VISIBLE_DEVICES=$GPU python -u main.py \
+CUDA_VISIBLE_DEVICES=$GPU python -W ignore -u main.py \
     --model_name $MODEL_NAME \
     --dataset_name $DATASET_NAME \
     --data_path $DATA_PATH \
     --litmodel_name $LITMODEL_NAME \
-    --max_epochs $MAX_EPOCHS \
+    --train_sampler_class $TRAIN_SAMPLER_CLASS \
+    --test_sampler_class $TEST_SAMPLER_CLASS \
+    --max_epochs $MAX_EPOCHES \
     --emb_dim $EMB_DIM \
-    --loss $LOSS \
-    --negative_adversarial_sampling \
+    --loss_name $LOSS_NAME \
     --train_bs $TRAIN_BS \
     --eval_bs $EVAL_BS \
     --num_neg $NUM_NEG \
-    --margin $MARGIN \
-    --dis_order $DIS_ORDER \
     --lr $LR \
     --regularization $REGULARIZATION \
     --check_per_epoch $CHECK_PER_EPOCH \
     --num_workers $NUM_WORKERS \
     --use_wandb \
+
 
