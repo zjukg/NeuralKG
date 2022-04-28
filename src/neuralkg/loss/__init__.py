e55f788d5c5ac04e7536970de1004a128b4c3844
Yao Zhen <43909501+Modberge@users.noreply.github.com>
2022-04-28 20:07:00 +0800

    Update __init__.py

--- src/neuralkg/loss/__init__.py
+++ src/neuralkg/loss/__init__.py
@@ -5,6 +5,5 @@ from .Cross_Entropy_Loss import Cross_Entropy_Loss
 from .RGCN_Loss import RGCN_Loss
 from .KBAT_Loss import KBAT_Loss
 from .CrossE_Loss import CrossE_Loss
-from .IterE_Loss import IterE_Loss
 from .Margin_Loss import Margin_Loss
 from .RugE_Loss import RugE_Loss
